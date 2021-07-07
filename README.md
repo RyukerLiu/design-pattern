4a73078c6e07cd069e48a5c66f0ac386916804e8
Allen Liu <alex03108861@gmail.com>
2021-07-07 18:02:11 +0800

    Make Factory

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Design Pattern
+
+## Creational Design Patterns
+
+https://refactoring.guru/design-patterns/creational-patterns
+
+### Abstract Factory
+
+https://refactoring.guru/design-patterns/abstract-factory
--- abstract_factory.py
+++ abstract_factory.py
@@ -1,1 +1,91 @@
-print('Hello World')
+class Chair:
+    pass
+
+
+class CoffeTable:
+    pass
+
+
+class Sofa:
+    pass
+
+
+class FurnitureFactory:
+    def create_chair(self) -> Chair:
+        raise NotImplementedError('.create_chair() should be overriden.')
+
+    def create_coffee_table(self) -> CoffeTable:
+        raise NotImplementedError(
+            '.create_coffee_table() should be overriden.')
+
+    def create_sofa(self) -> Sofa:
+        raise NotImplementedError('.create_sofa() should be overriden.')
+
+
+class VictorianChair(Chair):
+    pass
+
+
+class VictorianCoffeTable(CoffeTable):
+    pass
+
+
+class VictorianSofa(Sofa):
+    pass
+
+
+class VictorianFurnitureFactory:
+    def create_chair(self) -> Chair:
+        return VictorianChair()
+
+    def create_coffee_table(self) -> CoffeTable:
+        return VictorianCoffeTable()
+
+    def create_sofa(self) -> Sofa:
+        return VictorianSofa()
+
+
+class VictorianChair(Chair):
+    pass
+
+
+class VictorianCoffeTable(CoffeTable):
+    pass
+
+
+class VictorianSofa(Sofa):
+    pass
+
+
+class VictorianFurnitureFactory:
+    def create_chair(self) -> Chair:
+        return VictorianChair()
+
+    def create_coffee_table(self) -> CoffeTable:
+        return VictorianCoffeTable()
+
+    def create_sofa(self) -> Sofa:
+        return VictorianSofa()
+
+
+class ModernChair(Chair):
+    pass
+
+
+class ModernCoffeTable(CoffeTable):
+    pass
+
+
+class ModernSofa(Sofa):
+    pass
+
+
+class ModernFurnitureFactory:
+    def create_chair(self) -> Chair:
+        return ModernChair()
+
+    def create_coffee_table(self) -> CoffeTable:
+        return ModernCoffeTable()
+
+    def create_sofa(self) -> Sofa:
+        return ModernSofa()
