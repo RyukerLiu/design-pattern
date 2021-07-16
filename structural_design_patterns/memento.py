73e49d7a76e15178daa432016ad4714949e5c77c
Allen Liu <alex03108861@gmail.com>
2021-07-16 11:42:27 +0800

    Move to nested class

--- structural_design_patterns/memento.py
+++ structural_design_patterns/memento.py
@@ -1,23 +1,21 @@
 class Originator:
+    class Memento:
+        def restore(self):
+            raise NotImplementedError
+
     def create_memento(self):
         raise NotImplementedError
 
 
-class Memento:
-    def restore(self):
-        raise NotImplementedError
-
-
-class ScreenMemento(Memento):
-    def __init__(self, originator, display_content):
-        self.originator = originator
-        self.display_content = display_content
-
-    def restore(self):
-        self.originator.set_display_content(self.display_content)
-
-
 class Screen(Originator):
+    class ScreenMemento(Originator.Memento):
+        def __init__(self, originator, display_content):
+            self.originator = originator
+            self.display_content = display_content
+
+        def restore(self):
+            self.originator.set_display_content(self.display_content)
+
     def __init__(self, display_content):
         self.display_content = display_content
 
@@ -25,7 +23,7 @@ class Screen(Originator):
         self.display_content = display_content
 
     def create_memento(self):
-        memento = ScreenMemento(self, self.display_content)
+        memento = Screen.ScreenMemento(self, self.display_content)
         return memento
 
 
