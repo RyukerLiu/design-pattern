702c00bad60ac6de7b57886e73d8b283bcff71a9
Allen Liu <alex03108861@gmail.com>
2021-10-23 22:21:19 +0800

    Update Singleton

--- creational_design_patterns/singleton.py
+++ creational_design_patterns/singleton.py
@@ -1,35 +1,4 @@
-class Singleton:
-    __instance = None
-    __key = 'create_singleton'
-
-    def __init__(self, *args, create_key=None, **kwargs):
-        if create_key != self.__key:
-            raise Exception('Singleton should be construct by .get_instance')
-
-    @classmethod
-    def __construct(cls, *args, create_key=None, **kwargs):
-        if create_key != Singleton.__key:
-            raise Exception('Singleton should be construct by .get_instance')
-
-        return cls(*args, **kwargs, create_key=create_key)
-
-    @classmethod
-    def get_instance(cls, *args, **kwargs):
-        if cls.__instance is None:
-            cls.__instance = cls.__construct(
-                *args, **kwargs, create_key=cls.__key)
-
-        return cls.__instance
-
-
-class FileSingleton(Singleton):
-    def __init__(self, file, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.file = file
-
-    def get_file(self):
-        return self.file
-
+from threading import Lock
 
 class SingletonMeta(type):
     __instance = None
@@ -45,13 +14,43 @@ class SingletonMeta(type):
         return cls.__instance
 
 
-class SingletonV2(metaclass=SingletonMeta):
+class Singleton(metaclass=SingletonMeta):
     pass
 
 
-class FileSingletonV2(SingletonV2):
+class FileSingleton(Singleton):
     def __init__(self, file):
         self.file = file
 
     def get_file(self):
         return self.file
+
+
+class ThreadSafeSingletonMeta(type):
+    __instance = None
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        with cls._lock:
+            if cls.__instance is None:
+                instance = super().__call__(*args, **kwargs)
+                cls.__instance = instance
+        
+        return cls.__instance
+
+
+class ThreadSafeSingleton(metaclass=SingletonMeta):
+    pass
+
+
+class ThreadSafeFileSingleton(ThreadSafeSingleton):
+    def __init__(self, file):
+        self.file = file
+
+    def get_file(self):
+        return self.file
+
--- tests/creational_design_patterns_tests/test_singleton.py
+++ tests/creational_design_patterns_tests/test_singleton.py
@@ -1,54 +1,29 @@
 import unittest
-from creational_design_patterns.singleton import Singleton, FileSingleton, SingletonV2, FileSingletonV2
-
+from creational_design_patterns.singleton import Singleton, FileSingleton, ThreadSafeFileSingleton
 
 class TestSingleton(unittest.TestCase):
     def test_singleton(self):
-        s1 = Singleton.get_instance()
-        s2 = Singleton.get_instance()
+        s1 = Singleton()
+        s2 = Singleton()
 
         self.assertIs(s1, s2)
-
-    def test_singleton_private_construct(self):
-        with self.assertRaises(Exception) as cm:
-            s1 = Singleton()
-
-        exc = cm.exception
-        self.assertEqual(
-            str(exc), 'Singleton should be construct by .get_instance')
 
 
 class TestFileSingleton(unittest.TestCase):
     def test_singleton(self):
-        f1 = FileSingleton.get_instance('Test File')
-        f2 = FileSingleton.get_instance('Test File')
+        f1 = FileSingleton('Test File')
+        f2 = FileSingleton('TestXX')
 
         self.assertIs(f1, f2)
 
         file = f1.get_file()
         self.assertEqual(file, 'Test File')
 
-    def test_singleton_private_construct(self):
-        with self.assertRaises(Exception) as cm:
-            f1 = FileSingleton('Test File')
 
-        exc = cm.exception
-        self.assertEqual(
-            str(exc), 'Singleton should be construct by .get_instance')
-
-
-class TestSingletonV2(unittest.TestCase):
+class TestThreadSafeFileSingleton(unittest.TestCase):
     def test_singleton(self):
-        s1 = SingletonV2()
-        s2 = SingletonV2()
-
-        self.assertIs(s1, s2)
-
-
-class TestFileSingletonV2(unittest.TestCase):
-    def test_singleton(self):
-        f1 = FileSingletonV2('Test File')
-        f2 = FileSingletonV2('Test File')
+        f1 = ThreadSafeFileSingleton('Test File')
+        f2 = ThreadSafeFileSingleton('TestXX')
 
         self.assertIs(f1, f2)
 
