bedf3beac0d7041303dc4dd31d4ea1c52e121f13
Allen Liu <alex03108861@gmail.com>
2021-10-29 20:24:14 +0800

    New State Machine, inner state

--- behavioral_design_patterns/state.py
+++ behavioral_design_patterns/state.py
@@ -1,23 +1,20 @@
 class Context:
-    def __init__(self, state):
-        self.change_state(state)
-
-    def change_state(self, state):
+    def set_state(self, state):
         self.state = state
-        self.state.set_context(self)
 
 
 class Tv(Context):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.is_power_on = False
+    def __init__(self):
+        self.off_state = OffState(self)
+        self.on_state = OnState(self)
+        self.state = self.off_state
 
     def push_power_button(self):
         self.state.push_power_button()
 
 
 class State:
-    def set_context(self, context):
+    def __init__(self, context):
         self.context = context
 
     def push_power_button(self):
@@ -26,11 +23,9 @@ class State:
 
 class OnState(State):
     def push_power_button(self):
-        self.context.is_power_on = False
-        self.context.change_state(OffState())
+        self.context.set_state(self.context.off_state)
 
 
 class OffState(State):
     def push_power_button(self):
-        self.context.is_power_on = True
-        self.context.change_state(OnState())
+        self.context.set_state(self.context.on_state)
--- tests/behavioral_design_patterns_tests/test_state.py
+++ tests/behavioral_design_patterns_tests/test_state.py
@@ -4,14 +4,11 @@ from behavioral_design_patterns.state import Tv, OffState, OnState
 
 class TestState(unittest.TestCase):
     def test(self):
-
-        off = OffState()
-        tv = Tv(off)
+        tv = Tv()
+        self.assertEqual(type(tv.state), OffState)
 
         tv.push_power_button()
-        self.assertEqual(tv.is_power_on, True)
         self.assertEqual(type(tv.state), OnState)
 
         tv.push_power_button()
-        self.assertEqual(tv.is_power_on, False)
         self.assertEqual(type(tv.state), OffState)
